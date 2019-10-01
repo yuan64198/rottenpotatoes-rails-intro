990288cafe0097f39cea33dac4e6d63666076ebd
ChihYuanHuang <yuan64198@gmail.com>
2019-10-01 02:17:18 -0500

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,8 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @sort_column=params[:sort_by]
+    @movies = Movie.all.order(params[:sort_by])
   end
 
   def new
